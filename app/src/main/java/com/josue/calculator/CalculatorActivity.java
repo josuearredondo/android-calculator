0249e9681448750f637a006567a703bf80ae8be9
josuearredondo <josue.arredondo@hotmail.fr>
2016-10-04 15:43:53 +0200

    Added setOnClickListener method and set tag to the numbers

--- app/src/main/java/com/josue/calculator/CalculatorActivity.java
+++ app/src/main/java/com/josue/calculator/CalculatorActivity.java
@@ -3,6 +3,8 @@ package com.josue.calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,7 +17,7 @@ public class CalculatorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calculator);
 
         // find the elements
-        TextView etNum1 = (TextView) findViewById(R.id.etDigit);
+        final TextView tvDigit = (TextView) findViewById(R.id.tvDigit);
 
         Button btnDiv = (Button) findViewById(R.id.btnDiv);
         Button btnMult = (Button) findViewById(R.id.btnMult);
@@ -33,6 +35,45 @@ public class CalculatorActivity extends AppCompatActivity {
         Button btnSeven = (Button) findViewById(R.id.btnSeven);
         Button btnEight = (Button) findViewById(R.id.btnEight);
         Button btnNine = (Button) findViewById(R.id.btnNine);
+        /*btnDiv.setTag("/");
+        btnMult.setTag("x");
+        btnAdd.setTag("+");
+        btnSub.setTag("btnDiv");
+        btnComma.setTag("btnDiv");
+        btnEqual.setTag("btnDiv");*/
+        btnZero.setTag("0");
+        btnOne.setTag("1");
+        btnTwo.setTag("2");
+        btnThree.setTag("3");
+        btnFour.setTag("4");
+        btnFive.setTag("5");
+        btnSix.setTag("6");
+        btnSeven.setTag("7");
+        btnEight.setTag("8");
+        btnNine.setTag("9");
 
+
+        View.OnClickListener myClickButtonListener= new View.OnClickListener() {
+            public void onClick(View v) {
+                String tag = (String) v.getTag();
+                if (tvDigit.getText().equals("0")){
+                    tvDigit.setText("");
+                    tvDigit.append(tag);
+                } else {
+                    tvDigit.append(tag);
+                }
+            }
+        };
+
+        btnZero.setOnClickListener(myClickButtonListener);
+        btnOne.setOnClickListener(myClickButtonListener);
+        btnTwo.setOnClickListener(myClickButtonListener);
+        btnThree.setOnClickListener(myClickButtonListener);
+        btnFour.setOnClickListener(myClickButtonListener);
+        btnFive.setOnClickListener(myClickButtonListener);
+        btnSix.setOnClickListener(myClickButtonListener);
+        btnSeven.setOnClickListener(myClickButtonListener);
+        btnEight.setOnClickListener(myClickButtonListener);
+        btnNine.setOnClickListener(myClickButtonListener);
     }
 }
