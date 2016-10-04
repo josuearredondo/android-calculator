18bf79dff5af52d03ecd8d567c15841770e71f65
josuearredondo <josue.arredondo@hotmail.fr>
2016-10-04 16:46:22 +0200

    Added icons Calculator

--- app/src/main/java/com/josue/calculator/CalculatorActivity.java
+++ app/src/main/java/com/josue/calculator/CalculatorActivity.java
@@ -19,6 +19,7 @@ public class CalculatorActivity extends AppCompatActivity {
         // find the elements
         final TextView tvDigit = (TextView) findViewById(R.id.tvDigit);
 
+        Button btnC = (Button) findViewById(R.id.btnC);
         Button btnDiv = (Button) findViewById(R.id.btnDiv);
         Button btnMult = (Button) findViewById(R.id.btnMult);
         Button btnAdd = (Button) findViewById(R.id.btnSub);
@@ -35,12 +36,13 @@ public class CalculatorActivity extends AppCompatActivity {
         Button btnSeven = (Button) findViewById(R.id.btnSeven);
         Button btnEight = (Button) findViewById(R.id.btnEight);
         Button btnNine = (Button) findViewById(R.id.btnNine);
-        /*btnDiv.setTag("/");
-        btnMult.setTag("x");
+        btnC.setTag("c");
+        btnDiv.setTag("/");
+        btnMult.setTag("*");
         btnAdd.setTag("+");
-        btnSub.setTag("btnDiv");
-        btnComma.setTag("btnDiv");
-        btnEqual.setTag("btnDiv");*/
+        btnSub.setTag("-");
+        btnComma.setTag(".");
+        btnEqual.setTag("=");
         btnZero.setTag("0");
         btnOne.setTag("1");
         btnTwo.setTag("2");
@@ -60,11 +62,38 @@ public class CalculatorActivity extends AppCompatActivity {
                     tvDigit.setText("");
                     tvDigit.append(tag);
                 } else {
-                    tvDigit.append(tag);
+                    switch (tag) {
+                        case "c":
+                            tvDigit.setText("0");
+                            break;
+                        case "/":
+
+                            break;
+                        case "*":
+
+                            break;
+                        case "-":
+
+                            break;
+                        case "+":
+
+                            break;
+                        case ".":
+
+                            break;
+                        default: tvDigit.append(tag);
+                            break;
+                    }
                 }
             }
         };
-
+        btnC.setOnClickListener(myClickButtonListener);
+        btnDiv.setOnClickListener(myClickButtonListener);
+        btnMult.setOnClickListener(myClickButtonListener);
+        btnAdd.setOnClickListener(myClickButtonListener);
+        btnSub.setOnClickListener(myClickButtonListener);
+        btnComma.setOnClickListener(myClickButtonListener);
+        btnEqual.setOnClickListener(myClickButtonListener);
         btnZero.setOnClickListener(myClickButtonListener);
         btnOne.setOnClickListener(myClickButtonListener);
         btnTwo.setOnClickListener(myClickButtonListener);
