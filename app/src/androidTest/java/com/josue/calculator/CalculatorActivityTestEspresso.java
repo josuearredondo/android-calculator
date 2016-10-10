a6aa3a7b08d9e731cbd578cfcbdf15b5436e88c1
josuearredondo <josue.arredondo@hotmail.fr>
2016-10-10 12:08:12 +0200

    Added Test Espresso methods to test UI and Unit test

--- app/src/androidTest/java/com/josue/calculator/CalculatorActivityTestEspresso.java
+++ app/src/androidTest/java/com/josue/calculator/CalculatorActivityTestEspresso.java
@@ -5,7 +5,6 @@ import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
--- app/src/androidTest/java/com/josue/calculator/CalculatorActivityUITestEspresso.java
+++ app/src/androidTest/java/com/josue/calculator/CalculatorActivityUITestEspresso.java
@@ -20,7 +20,6 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
